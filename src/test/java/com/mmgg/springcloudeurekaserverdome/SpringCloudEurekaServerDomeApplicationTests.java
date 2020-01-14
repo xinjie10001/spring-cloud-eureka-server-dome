f3a25b6d5062dd2bcc27074d6fc6544f143b6451
wangxinjie <wangxinjie@ky-tech.com.cn>
2020-01-14 18:36:31 +0800

    first commit

---
+++ src/test/java/com/mmgg/springcloudeurekaserverdome/SpringCloudEurekaServerDomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmgg.springcloudeurekaserverdome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudEurekaServerDomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
